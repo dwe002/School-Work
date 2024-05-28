c1575b22474e8589b623a20d56c1ade2e93f71c6
Daniel Delgado Acosta <dwe007722@gmail.com>
2024-05-28 16:01:56 -0700

    Delete cse 5208/LABS directory

--- cse 5208/LABS/Delgado_Daniel_lab1/lab1_p1.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-name = input('What is your name?: ')
-age = int(input('What is your age?: '))
-
-year = date.today()
-DOB = year.year - age
-
-print('Name & DOB:', name, DOB)
--- cse 5208/LABS/Delgado_Daniel_lab2/lab2_p1.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open("students.csv", "r") as students:
-	read_file = csv.reader(students) #assigns csv file to read_file
-	
-	for s in read_file:
-		 print(f'{s[0]}     {s[1]}      {s[2]}      {s[3]}') #prints each word in the csv file 
